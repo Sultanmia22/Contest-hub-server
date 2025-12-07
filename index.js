712cbf927af24cf2d985c845d1be95f36357671e
Sultan Mia <sultanmia5732@gmail.com>
2025-12-07 14:59:35 +0600

    create user data insert api

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const admin = require("firebase-admin");
 const port = 3000
 
 // Midleware
 app.use(express.json())
 app.use(cors())
+
+
+
+//firebase sdk 
+const serviceAccount = require('./contesthub-firebase-adminsdk.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// Verify firebase toekn 
+const verifyFbToken = async(req,res,next) => {
+   const accessToken = req.headers.authorization;
+   const token = accessToken.split(' ')[1];
+   if (!token) return res.status(401).send({ message: 'Unauthorized Access!' });
+
+   try{
+      const decoded = await admin.auth().verifyIdToken(token);
+     req.tokenEmail = decoded.email;
+     next()
+   }
+   catch(er){
+    console.log(er)
+    return res.status(401).send({ message: 'Unauthorized Access!', err })
+   }
+}
 
 
 // mongodb client and uri
@@ -24,6 +51,26 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const db = client.db('contestHub-db');
+    const userCollection = db.collection('users')
+
+    //! all user releted api here 
+
+    // insert user data in database
+    app.post('/user',async(req,res) => {
+      try{
+        const userData = req.body;
+        userData.role = 'user',
+        userData.createdAt = new Date();
+        const result = await userCollection.insertOne(userData);
+        res.json(result)
+      }
+      catch(er) {
+        console.log(er)
+        res.json(er)
+      }
+    })
    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
