e9dcaadda53470a3a825927b83d1aea7a3fa40e3
Sultan Mia <sultanmia5732@gmail.com>
2025-12-19 19:21:11 +0600

    update sdk key setup

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(cors())
 
 
 //firebase sdk 
-const serviceAccount = require('./contesthub-firebase-adminsdk.json');
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
